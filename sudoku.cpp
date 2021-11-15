88051be033618f51c7ff389f3a7730474d59d66a
Marco Ybanez <marcoybanez@wlan-t-120-96-dhcp.CSUChico.EDU>
2021-11-15 14:58:47 -0800

    Added Brandpn's files

---
+++ sudoku.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <math.h>
+using namespace std;
+
+class sudoku {
+    private:
+        int size; // number of rows Ex. 16
+        int sizePerBox; // number inside a box Ex. 4
+        bool checkRow(); //check if a number is valid in a row
+        bool checkColumn(); //check if a number is valid in a column
+        bool checkBox(); //check if a number is valid in a box
+        vector<vector<int> > board;  // holds the sudoku
+    public:
+        sudoku(int N);
+        bool dfsVisit(int row, int column);
+        void printBoard();
+};
+
+sudoku::sudoku(int N)
+:size(N * N), sizePerBox(N) {
+    board.resize(size);
+    int tempInt;
+    for(int i = 0 ; i < size; i++) {
+        for(int j = 0; j < size; j++) {
+            cin >> tempInt;
+            board[i].push_back(tempInt);
+        }
+    }
+}
+
+void sudoku::printBoard() {
+    cout << "------------------------------------------" << endl;
+    for(int i = 0 ; i < size; i++) {
+        cout << "|";
+        for(int j = 0; j < size; j++) {
+            cout << board[i][j] << " ";
+            if(j % sizePerBox - 3 == 0) {
+                cout << "|";
+            }
+        }
+        if(i % sizePerBox - 3 == 0) {
+            cout << endl;
+            cout << "------------------------------------------";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int N;
+    cin >> N;
+    sudoku mySudoku(N);
+    // if(!mySudoku.dfsVisit(0,0)) {
+    //     cout << "There is not a valid solution." << endl;
+    // } else {
+    //     mySudoku.printBoard();
+    // }
+    mySudoku.printBoard();
+    return 0;
+}
