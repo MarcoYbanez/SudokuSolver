2044b3f682f21f5c6f48a4e8ae59cd4b30630644
blueweaver <brandonweaver2011@hotmail.com>
2021-12-04 23:13:03 -0800

    separate the display by their square

--- display.py
+++ display.py
@@ -2,6 +2,7 @@
 from tkinter import Tk, Canvas, Frame, BOTH, Button, Label, IntVar
 import fileinput
 import os
+from tkinter.constants import TRUE
 
 class Example(Frame):
     def __init__(self, parent, a, container):
@@ -34,19 +35,35 @@ class Example(Frame):
         self.pack(fill=BOTH, expand=1)
         self.canvas.pack(fill=BOTH, expand=1)
         itor = 0
+        spacerX = 0
+        spacerY = 0
+        readyX = True
+        readyY = True
         for i in range(n):
+            spacerY = 0
+            if(i == 5 or i == 9 or i == 13):
+                readyX = True
+            if((i == 4 or i == 8 or i == 12) and readyX):
+                spacerX += 20
+                readyX = False
             for j in range(n):
-                self.cellwidth = 20
-                self.cellheight = 20
-                x1 = (i * self.cellwidth) + 5
-                y1 = (j * self.cellheight) + 5
-                x2 = x1 + self.cellwidth
+                self.cellwidth = 30
+                self.cellheight = 30
+                if(j == 5 or j == 9 or j == 13):
+                     readyY = True
+                if((j == 4 or j == 8 or j == 12) and readyY):
+                     spacerY += 20
+                     readyY = False
+                x1 = (i * self.cellwidth) + 10 + spacerX
+                y1 = (j * self.cellheight) + 10 + spacerY
+                x2 = x1 + self.cellwidth 
                 y2 = y1 + self.cellheight
-                self.canvas.create_rectangle(x1+2,y1+2,x2,y2, outline="#ff0000")
+                #self.canvas.create_rectangle(x1+2,y1+2,x2,y2, outline="#ff0000")
+                self.canvas.create_rectangle(x1+2,y1+2,x2,y2)
                 if(str(self.a[j][i]) != "0"):
                     self.container[itor] = self.canvas.create_text(x2-6,y2-6, text=str(self.a[j][i]))
                 else:
-                    self.container[itor] = self.canvas.create_text(x2-6,y2-6, fill="#000fff000", text=str(self.a[j][i]))
+                    self.container[itor] = self.canvas.create_text(x2-6,y2-6, fill="#990099", text=str(self.a[j][i]))
                 itor += 1
 
     def solve(self):
